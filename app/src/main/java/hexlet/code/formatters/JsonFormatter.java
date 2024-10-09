6662ee52eca8aca14672cdc60890bcbecc005972
Daniil Kirillov <danikirillov@ya.ru>
2024-10-09 14:36:42 +0200

    Fix json format

--- app/src/main/java/hexlet/code/formatters/JsonFormatter.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -3,26 +3,12 @@ package hexlet.code.formatters;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hexlet.code.model.DiffLine;
-import hexlet.code.model.JsonAddedNode;
-import hexlet.code.model.JsonDiffNode;
-import hexlet.code.model.JsonRemovedNode;
 
 import java.util.List;
-import java.util.TreeMap;
 
 public class JsonFormatter {
     public static String format(List<DiffLine> diff) throws JsonProcessingException {
         var om = new ObjectMapper();
-        var statusToChanges = new TreeMap<String, Object>();
-        for (var diffLine : diff) {
-            var jsonDiff = switch (diffLine.status()) {
-                case ADDED -> new JsonAddedNode(diffLine.newValue());
-                case UPDATED, UNTOUCHED -> new JsonDiffNode(diffLine.oldValue(), diffLine.newValue());
-                case REMOVED -> new JsonRemovedNode(diffLine.oldValue());
-                default -> throw new IllegalStateException("Unexpected value: " + diffLine.status());
-            };
-            statusToChanges.put(diffLine.name(), jsonDiff);
-        }
-        return om.writeValueAsString(statusToChanges);
+        return om.writeValueAsString(diff);
     }
 }
--- app/src/main/java/hexlet/code/model/JsonRemovedNode.java
+++
@@ -1,4 +0,0 @@
-package hexlet.code.model;
-
-public record JsonRemovedNode(Object onlyOldValue) {
-}
